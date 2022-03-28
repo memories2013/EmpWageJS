1fa8f880917eb77fbfcd795f2e304ffc1e8beb47
memories2013 <nshivaprasad41@gmail.com>
2022-03-28 19:55:46 +0530

    UC-3:- Write Function to get work hours.

--- EmpWage.js
+++ EmpWage.js
@@ -1,9 +1,24 @@
 const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const EMP_WAGE_PHR = 20;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
 
-let empCheck = Math.floor(Math.random() * 2);
-if (empCheck == IS_ABSENT) {
-  console.log("Employee is absent.");
-} else {
-  console.log("Employee is presnt.");
+function getEmpHrs(empCheck) {
+  switch (empCheck) {
+    case IS_PART_TIME:
+      return PART_TIME_HRS;
+      break;
+    case IS_FULL_TIME:
+      return FULL_TIME_HRS;
+      break;
+    default:
+      return IS_ABSENT;
+  }
 }
- 
+
+let empCheck = Math.floor(Math.random() * 3);
+
+let empWage = getEmpHrs(empCheck) * EMP_WAGE_PHR; //Calculating wage.
+console.log("Employee Wage :- " + empWage);
